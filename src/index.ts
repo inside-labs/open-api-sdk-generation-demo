1476e3d50101806c9a6f0aa112202159d354fc60
Manuel Alabor <manuel@insidelabs.tech>
2024-08-26 15:21:19 +0200

    Better Example Call

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { createApiClient } from "./lib/sdk/cariboo/client.js";
 
+const SURSELVA_PRODUCTION_URL = "https://surselva.cariboo.tech";
+
 try {
-  const client = createApiClient("https://surselva.cariboo.tech");
-  const result = await client.getSkiticketData();
-  console.log(JSON.stringify(result, null, 2));
+  const cariboo = createApiClient(SURSELVA_PRODUCTION_URL);
+
+  const offers = await cariboo.getPrices({
+    id: 801, // Wanderticket, Brigels Burleun Retour
+    persons: [{ person_id: 2, quantity: 1 }], // 1x Erwachsen
+    from: "2024-09-20",
+    to: "2024-09-22",
+  });
+
+  console.log(JSON.stringify(offers, null, 2));
 } catch (e) {
   console.error(JSON.stringify(e));
 }
